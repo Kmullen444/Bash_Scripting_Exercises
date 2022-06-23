2e11338ec04836573b621716236c497d4f572312
Kevin <64184097+Kmullen444@users.noreply.github.com>
2022-06-23 16:31:39 -0700

    Added execises

--- medium_exercises/exe12.sh
+++ medium_exercises/exe12.sh
@@ -4,10 +4,21 @@ files in the present working directory. Name this fuction "file_count" and call 
 in your script. If you use a variable in your fucntion remember to make it a local 
 variable'
 
+: '
+Modify the script:
+Make the "file_count" function accept a directory as an argument. Nex, have the
+function display the name fo the directory followed by a colon. Finally display
+the number of files to the screen on the next line. Call the function three times.
+First on the "/etc" directory, next on "/var" directory, finally on "/usr/bin" directory'
+
 #! /bin/bash
 
 file_count () {
-  echo $( ls -l | wc -l )
+  local DIRECT=$1
+  echo $DIRECT :
+  echo $( ls -l $DIRECT | wc -l )
 }
 
-file_count
+file_count /etc
+file_count /var
+file_count /usr/bin
