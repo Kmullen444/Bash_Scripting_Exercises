9d32b5fa004faf8b5ccc93381b76c7d9b54cca60
Kevin <64184097+Kmullen444@users.noreply.github.com>
2022-06-29 18:10:30 -0700

    Finished exercises in Python

--- medium_exercises/python_shell/exe6.py
+++ medium_exercises/python_shell/exe6.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+import os
+
+file =  input("Enter a file path: ")
+
+if os.path.isdir(file):
+    print(f'{file} is a Directory')
+elif os.path.is_file(file):
+    print(f'{file} is a File')
+else:
+    print(f'{file} is another type of file')
+
+print(os.system(f'{file} ls -l'))
